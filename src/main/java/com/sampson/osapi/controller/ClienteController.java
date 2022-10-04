32d013950d147f3ea1962642952736862862dec4
fsampson30 <fsampson30@gmail.com>
2022-10-04 09:36:00 -0300

    Created method to list Clientes from Database.

--- src/main/java/com/sampson/osapi/controller/ClienteController.java
+++ src/main/java/com/sampson/osapi/controller/ClienteController.java
@@ -4,26 +4,19 @@ import com.sampson.osapi.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        var cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Fernanda");
-        cliente1.setTelefone("21 999999999");
-        cliente1.setEmail("a@a.com");
-
-        var cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Natalia");
-        cliente2.setTelefone("21 888888888");
-        cliente2.setEmail("b@b.com");
-
-        return Arrays.asList(cliente1, cliente2);
+        return manager.createQuery("from Cliente", Cliente.class).getResultList();
     }
 }
--- src/main/java/com/sampson/osapi/domain/model/Cliente.java
+++ src/main/java/com/sampson/osapi/domain/model/Cliente.java
@@ -1,7 +1,16 @@
 package com.sampson.osapi.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
@@ -37,5 +46,18 @@ public class Cliente {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return id.equals(cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
