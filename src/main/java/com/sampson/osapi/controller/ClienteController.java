097d378afdca58a83eaa1293cf4e426fe9b24ff1
fsampson30 <fsampson30@gmail.com>
2022-10-04 11:36:55 -0300

    Created ClienteRepository.java

--- src/main/java/com/sampson/osapi/controller/ClienteController.java
+++ src/main/java/com/sampson/osapi/controller/ClienteController.java
@@ -1,22 +1,21 @@
 package com.sampson.osapi.controller;
 
 import com.sampson.osapi.domain.model.Cliente;
+import com.sampson.osapi.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        return manager.createQuery("from Cliente", Cliente.class).getResultList();
+        return clienteRepository.findAll();
     }
 }
