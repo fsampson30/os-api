10705fa5f1d840d6dfe8a7e4c3097dc5ea824fbb
fsampson30 <fsampson30@gmail.com>
2022-10-07 13:14:50 -0300

    Added bean validation.

--- src/main/java/com/sampson/osapi/domain/model/Cliente.java
+++ src/main/java/com/sampson/osapi/domain/model/Cliente.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
@@ -12,8 +15,16 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     private String nome;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(max = 20)
     private String telefone;
 
     public Long getId() {
