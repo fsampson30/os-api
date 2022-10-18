bd5ca6476d2fe5dd9184b5443f7a3a4ef6b34db8
fsampson30 <fsampson30@gmail.com>
2022-10-18 08:12:29 -0300

    Created logic to list Comentario.

--- src/main/java/com/sampson/osapi/controller/ComentarioController.java
+++ src/main/java/com/sampson/osapi/controller/ComentarioController.java
@@ -1,6 +1,9 @@
 package com.sampson.osapi.controller;
 
+import com.sampson.osapi.domain.exception.EntidadeNaoEncontradaException;
 import com.sampson.osapi.domain.model.Comentario;
+import com.sampson.osapi.domain.model.OrdemServico;
+import com.sampson.osapi.domain.repository.OrdemServicoRepository;
 import com.sampson.osapi.domain.service.GestaoOrdemServicoService;
 import com.sampson.osapi.model.ClienteResumoModel;
 import com.sampson.osapi.model.ComentarioInputModel;
@@ -11,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("ordens-servico/{ordemServicoId}/comentarios")
@@ -21,6 +26,18 @@ public class ComentarioController {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+
+    @GetMapping
+    public List<ComentarioModel> listar(@PathVariable Long ordemServicoId) {
+        OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException("Ordem de servico não encontrada"));
+        return toCollectionModel(ordemServico.getComentarios());
+    }
+
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -34,5 +51,10 @@ public class ComentarioController {
 
     private ComentarioModel toModel(Comentario comentario) {
         return modelMapper.map(comentario, ComentarioModel.class);
+    }
+
+    private List<ComentarioModel> toCollectionModel(List<Comentario> comentarios) {
+        return comentarios.stream().map(comentario -> toModel(comentario)).collect(Collectors.toList());
+
     }
 }
